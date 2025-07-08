b40f7162afef61e25ccd00e54f0bf2192b40ba36
salman <salmankhan.cw@gmail.com>
2025-07-08 12:47:35 +0500

    super user added

---
+++ home/management/commands/createadmin.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        username = 'salman-khan'
+        email = 'salmankhan.cw@gmail.com'
+        password = 'p2g00971'
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, email=email, password=password)
+            self.stdout.write(self.style.SUCCESS('Superuser created successfully.'))
+        else:
+            self.stdout.write(self.style.WARNING('Superuser already exists.'))
