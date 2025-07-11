ee26d9f19f941704369aa36d562faa92957f3f61
salman <salmankhan.cw@gmail.com>
2025-07-11 15:58:20 +0500

    correct resume model

--- home/models/Resume.py
+++ home/models/Resume.py
@@ -4,4 +4,4 @@ class Resume(models.Model):
     file =models.URLField(blank=True, null=True)
 
     def __str__(self):
-        return self.file.name
+        return self.file or "No Resume Uploaded"
